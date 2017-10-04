79dfc76294416216bc249d7080145a219bfa2c23
Eugene Stankevich <stankevich.e@gmail.com>
2017-10-04 18:32:37 +0300

    update react-native plugin to 3.1.0

--- rules/react-native.js
+++ rules/react-native.js
@@ -16,16 +16,16 @@ module.exports = {
 	// https://github.com/intellicode/eslint-plugin-react-native#list-of-supported-rules
 	rules: {
 		// Detect StyleSheet rules which are not used in your React components
-		"react-native/no-unused-styles": 2,
+		"react-native/no-unused-styles": "error",
 
 		// Enforce using platform specific filenames when necessary
-		"react-native/split-platform-components": 2,
+		"react-native/split-platform-components": "error",
 
 		// Detect JSX components with inline styles that contain literal values
-		"react-native/no-inline-styles": 2,
+		"react-native/no-inline-styles": "error",
 
 		// Detect StyleSheet rules and inline styles containing color literals instead of variables
-		"react-native/no-color-literals": 2
+		"react-native/no-color-literals": "error"
 	},
 
 	settings: {
