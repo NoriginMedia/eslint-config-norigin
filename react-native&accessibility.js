bedef3ffc52050b4b5781a37f3706c8b96db597f
Eugene Stankevich <stankevich.e@gmail.com>
2017-10-05 11:03:24 +0300

    move accessibility to separate react-native config

--- react-native&accessibility.js
+++ react-native&accessibility.js
@@ -2,7 +2,8 @@ module.exports = {
 	extends: [
 		"./index",
 		"./rules/react",
-		"./rules/react-native"
+		"./rules/react-native",
+		"./rules/react-a11y"
 	].map(require.resolve),
 	rules: {}
 };
